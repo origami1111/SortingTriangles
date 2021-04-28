2df8a01b6ab6c3890c81e45367a7fba3c556e971
Вячеслав Островский <slslava007@gmail.com>
2021-04-28 16:35:22 +0300

    Add project files.

---
+++ SortingTriangles/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTriangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            bool flag = true;
+
+            string input;
+            string[] parameters;
+
+            string name = "";
+            double a = 0.0, b = 0.0, c = 0.0;
+
+            Triangle triangle;
+            List<Triangle> triangles = new List<Triangle>();
+
+            do
+            {
+                while (flag)
+                {
+                    Console.Write("Формат ввода (разделитель - запятая): <имя>, <длина стороны>, <длина стороны>, <длина стороны> \n > ");
+                    input = Console.ReadLine();
+
+                    parameters = input.Split(',');
+
+                    name = parameters[0].Trim();
+                    try
+                    {
+                        a = double.Parse(parameters[1], CultureInfo.InvariantCulture);
+                        b = double.Parse(parameters[2], CultureInfo.InvariantCulture);
+                        c = double.Parse(parameters[3], CultureInfo.InvariantCulture);
+
+                        flag = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+
+
+                triangle = new Triangle(name, a, b, c);
+                triangles.Add(triangle);
+
+                Console.Write("Продолжить ввод(y/yes)? > ");
+                answer = Console.ReadLine().ToLower();
+            } while (answer == "y" || answer == "yes");
+
+            Console.WriteLine("============= Triangles list: ===============");
+            triangles.Sort();
+            for (int i = 0; i < triangles.Count; i++)
+                Console.WriteLine($"{i + 1}. {triangles[i]}");
+
+            Console.WriteLine("Программа завершена");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SortingTriangles/Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTriangles
+{
+    class Triangle : IComparable<Triangle>
+    {
+        private string name;
+        private double a;
+        private double b;
+        private double c;
+
+        public Triangle() { }
+        public Triangle(string name, double a, double b, double c)
+        {
+            this.name = name;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public int CompareTo(Triangle other)
+        {
+            if (this.getArea() > other.getArea())
+                return -1;
+            if (this.getArea() < other.getArea())
+                return 1;
+            else
+                return 0;
+        }
+
+        public double getArea()
+        {
+            double p = 0.5 * (a + b + c);
+
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public override string ToString()
+        {
+            return $"[Triangle {name}]: {Math.Round(getArea(), 2)} cm";
+        }
+
+    }
+}
